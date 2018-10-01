7dc701f3e632528832ba1923bc07db1e2986a7e8
Kamal <nouary@live.com>
2018-10-01 20:46:29 +0200

    database query function

--- controller/DataBaseController.php
+++ controller/DataBaseController.php
@@ -16,13 +16,24 @@ class DataBaseController {
             $this->dbname = $dbname;
         }
 
-
         try {
-            $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname.'', $this->user, $this->pass);
+            $this->pdo = new PDO('mysql:host='.$this->host.';dbname='.$this->dbname.'', $this->user, $this->pass,
+            array(
+                PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES UTF8',
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false
+            ));
 
         } catch(Exception $e) {
             die($e);
         }
     }
+
+
+    public  function q($sql, $data=array()) {
+        $q = $this->pdo->prepare($sql);
+        $q->execute($data);
+        return $q->fetchAll(PDO::FETCH_OBJ);
+      }
 
 }
